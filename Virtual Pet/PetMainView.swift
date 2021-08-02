45e11245ffbb3baa483f057d29e5ae8a1a0734fe
Cynthia Wang <CynthiaWang7979@hotmail.com>
2021-08-03 01:53:15 +0800

    Created PetView

---
+++ Virtual Pet/PetMainView.swift
@@ -0,0 +1,39 @@
+//
+//  PetMainView.swift
+//  Virtual Pet
+//
+//  Created by Xia He on 2021/8/3.
+//
+//  Commented code are undetermined parts
+
+import SwiftUI
+
+struct PetMainView: View {
+    // let petManager: PetManager
+    var body: some View {
+        GeometryReader { metric in
+            VStack {
+                VStack {  // Top bar
+                    
+                }
+                .frame(width: metric.size.width, height: metric.size.height*0.1)
+                
+                VStack {  // Main content
+                    // Image(petManager.petType+"idle")
+                }
+                .frame(width: metric.size.width, height: metric.size.height*0.8)
+                
+                VStack {  // Bottom bar
+                    
+                }
+                .frame(width: metric.size.width, height: metric.size.height*0.1)
+            }
+        }
+    }
+}
+
+struct PetMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        PetMainView()
+    }
+}
