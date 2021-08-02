bbccf7a3c0450025be3a357594648a3e06c131b8
AppreciativeGeek <mnpencildoodle@gmail.com>
2021-08-02 10:52:06 -0500

    Initial Commit

---
+++ Virtual Pet/Virtual_PetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Virtual_PetApp.swift
+//  Virtual Pet
+//
+//  Created by James on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Virtual_PetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
