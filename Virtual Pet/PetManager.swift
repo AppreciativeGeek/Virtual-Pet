08c06093103d38b30b0899a4f8db694551a3a0ae
AppreciativeGeek <mnpencildoodle@gmail.com>
2021-08-05 12:41:58 -0500

    Added persistent data to petName, userName, and petType.

--- Virtual Pet/PetManager.swift
+++ Virtual Pet/PetManager.swift
@@ -9,28 +9,76 @@ import Foundation
 
 class PetManager : ObservableObject {
     // Default values for per-page canvas
-    @Published var petName: String
-    @Published var userName: String
-    @Published var petType: String
+    @Published var petName: String {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(petName) {
+                UserDefaults.standard.set(encoded, forKey: "petName")
+            }
+        }
+    }
+    @Published var userName: String {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(userName) {
+                UserDefaults.standard.set(encoded, forKey: "userName")
+            }
+        }
+    }
+    @Published var petType: String {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(petType) {
+                UserDefaults.standard.set(encoded, forKey: "petType")
+            }
+        }
+    }
     @Published var hungerStatus = 50
     @Published var energyStatus = 50
     @Published var joyStatus = 50
     
     init(petName: String = "", userName: String = "", petType: String = "dog") {
-        self.petName = petName
-        self.userName = userName
-        self.petType = petType
+        self.petName = ""
+        self.userName = ""
+        self.petType = ""
+        
+        if let petName = UserDefaults.standard.data(forKey: "petName") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode(String.self, from: petName) {
+                self.petName = decoded
+            }
+        } else {
+            self.petName = petName
+        }
+
+        if let userName = UserDefaults.standard.data(forKey: "userName") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode(String.self, from: userName) {
+                self.userName = decoded
+            }
+        } else {
+            self.userName = userName
+        }
+        
+        if let petType = UserDefaults.standard.data(forKey: "petType") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode(String.self, from: petType) {
+                self.petType = decoded
+            }
+        } else {
+            self.petType = petType
+        }
     }
     
     public func manageStatus() {
         Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { timer in
-            if self.hungerStatus <= 100 && self.hungerStatus > 0 {
+            if (self.hungerStatus <= 100) && (self.hungerStatus > 0) {
                 self.hungerStatus -= 1
             }
-            if self.energyStatus <= 100 && self.energyStatus > 0 {
+            if (self.energyStatus <= 100) && (self.energyStatus > 0) {
                 self.energyStatus -= 1
             }
-            if self.joyStatus <= 100 && self.joyStatus > 0 {
+            if (self.joyStatus <= 100) && (self.joyStatus > 0) {
                 self.joyStatus -= 1
             }
         }
