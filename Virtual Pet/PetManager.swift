5703f80c0fdebcbdad309aacf231c2bf1bf50554
Cynthia Wang <CynthiaWang7979@hotmail.com>
2021-08-05 23:06:54 +0800

    Fixed func public bug

--- Virtual Pet/PetManager.swift
+++ Virtual Pet/PetManager.swift
@@ -22,7 +22,7 @@ class PetManager : ObservableObject {
         self.petType = petType
     }
     
-    func manageStatus() {
+    public func manageStatus() {
         Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { timer in
             if self.hungerStatus < 100 && self.hungerStatus >= 0 {
                 self.hungerStatus += 1
