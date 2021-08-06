f30a73258fd3f73422d2bebb79c2266a2aa8c387
Cynthia Wang <CynthiaWang7979@hotmail.com>
2021-08-06 22:15:01 +0800

    Unified code style

--- Virtual Pet/LoginView.swift
+++ Virtual Pet/LoginView.swift
@@ -13,17 +13,23 @@ struct LoginView: View {
     var body: some View {
         VStack {
             Text("What is your name?")
+            
             CustomTextField(placeholder: "Your name", variable: $petManager.userName)
                 .padding()
+            
             Text("What would you like your pet's name to be?")
+            
             CustomTextField(placeholder: "Your pet's name", variable: $petManager.petName)
                 .padding()
+            
             Text("What kind of pet would you like to have?")
+            
             Picker("", selection: $petManager.petType) {
                 Text("Dog").tag("dog")
             }
             .pickerStyle(SegmentedPickerStyle())
             .padding()
+            
             NavigationLink("Proceed", destination: PetMainView(petManager: petManager))
         }
         .frame(width: 360, height: 200, alignment: .center)
